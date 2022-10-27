592f7d852be8b7c29ed385fc510d69fc3de117b2
Igor.Sokolov <I.Sokolov@ya.ru>
2022-10-27 10:54:24 +0400

    add pre-build ifcEngine

--- CS_ExpressApiExamples/Main.cs
+++ CS_ExpressApiExamples/Main.cs
@@ -9,11 +9,8 @@ namespace CS_IFC
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            CS_IFC.IFC4_test();
-            IfcBlobTextureExample.Run();
             HelloWall.Run();
             GuideExample.Run();
-            CS_AP242.Run();
         }
     }
 }
