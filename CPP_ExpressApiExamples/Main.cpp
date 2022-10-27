abf4baf45ab17a9c92b1c7f4da611bd4c033e3c4
Igor.Sokolov <I.Sokolov@ya.ru>
2022-10-27 08:02:47 +0400

    remove tests

--- CPP_ExpressApiExamples/Main.cpp
+++ CPP_ExpressApiExamples/Main.cpp
@@ -1,18 +1,9 @@
 
-extern void IFC4_test();
-extern void AP242_test();
 extern void HelloWall();
 extern void GuideExamples();
-extern void EngineTests(void);
 
 extern int main()
 {
-    EngineTests();
-
-    IFC4_test();
-
-    AP242_test();
-
     HelloWall();
 
     GuideExamples();
